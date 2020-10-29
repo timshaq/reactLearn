05a8e8d3380678a8e7f0920ae698bbe3aa4ec213
testtimshaq <58642417+testtimshaq@users.noreply.github.com>
2020-10-29 13:52:56 +0500

    hmr

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,9 +1,13 @@
+import { hot } from 'react-hot-loader/root';
 import * as React from 'react';
 
-export function Header() {
+function HeaderComponent() {
   return (
     <header>
       <h1>H1 Header</h1>
+      <button>Click</button>
     </header>
   )
 };
+
+export const Header = hot(HeaderComponent);
